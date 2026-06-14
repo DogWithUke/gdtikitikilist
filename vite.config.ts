4221fb2a41c4b35ced3e8dc28667cad6d9b6786e
DogWithUke <przemekwojik@gmail.com>
2026-06-14 20:18:34 +0200

    vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
-tanstackStart: {
-server: { entry: "server" },
-},
-vite: {
-plugins: [],
-},
+  vite: {
+    plugins: [],
+  },
+  tanstackStart: {
+    server: {
+      entry: "server",
+    },
+  },
 });
